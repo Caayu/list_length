6f23afa085ef3845b285eff292821179e92a1f37
caayu <henrycay@outlook.com>
2021-09-17 13:04:25 -0300

    test: create function to counter list lengt

--- test/list_length_test.exs
+++ test/list_length_test.exs
@@ -1,8 +1,25 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "returns the count of the number of items in the list" do
+      list = [1, 7, 9, 3]
+
+      response = ListLength.call(list)
+
+      expected_response = 4
+
+      assert response == expected_response
+    end
+
+    test "retun 0 if passing an empty" do
+      list = []
+
+      response = ListLength.call(list)
+
+      expected_response = 0
+
+      assert response == expected_response
+    end
   end
 end
